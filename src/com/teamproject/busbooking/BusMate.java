707eb7ba831e414d0fb2cad21becb36d7686bafd
Kutraleeswaran B <vtu12024@veltech.edu.in>
2025-11-14 20:26:28 +0530

    ui update

--- src/com/teamproject/busbooking/BusMate.java
+++ src/com/teamproject/busbooking/BusMate.java
@@ -7,7 +7,9 @@ public class BusMate {
     public static final String version= "0.0.2";
 
     public static void main(String[] args) {
+        System.out.println("-----------------------------------------");
         System.out.println("Welcome to BusMate - " +  version);
+        System.out.println("-----------------------------------------");
         WelcomeMenu welcomeMenu = new WelcomeMenu();
         welcomeMenu.displayMenu();
     }
