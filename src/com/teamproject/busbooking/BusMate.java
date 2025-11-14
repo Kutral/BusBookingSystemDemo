1c48819fa3fd4147bfb68ccff31e1759dc9ec540
Kutraleeswaran B <vtu12024@veltech.edu.in>
2025-11-14 20:30:42 +0530

    trying busrepo

--- src/com/teamproject/busbooking/BusMate.java
+++ src/com/teamproject/busbooking/BusMate.java
@@ -2,7 +2,6 @@ package com.teamproject.busbooking;
 
 import com.teamproject.busbooking.view.WelcomeMenu;
 
-
 public class BusMate {
     public static final String version= "0.0.2";
 
--- src/com/teamproject/busbooking/data/BusRepository.java
+++ src/com/teamproject/busbooking/data/BusRepository.java
@@ -1,4 +1,11 @@
 package com.teamproject.busbooking.data;
 
+import com.teamproject.busbooking.model.Bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusRepository {
+    private final List<Bus> bus = new ArrayList<>();
+
 }
